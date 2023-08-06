7bda28504e597eed189927bb0fb25601257dbfdf
Zeynep <zeynep@192.168.1.6>
2023-08-06 21:20:40 +0300

    Anasayfa degistirildi, odev son haline getirildi.

---
+++ toDoApp/ui/tableview/TasksHucre.swift
@@ -0,0 +1,22 @@
+//
+//  TasksHucre.swift
+//  toDoApp
+//
+//  Created by Zeynep on 6.08.2023.
+//
+
+import UIKit
+
+class TasksHucre: UITableViewCell {
+
+    @IBOutlet weak var labelTaskName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- toDoApp/ui/views/Anasayfa.swift
+++ toDoApp/ui/views/Anasayfa.swift
@@ -9,16 +9,23 @@ import UIKit
 
 class Anasayfa: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tasksTableView: UITableView!
+    
+    var tasksListesi = [Tasks]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        tasksTableView.delegate = self
+        tasksTableView.dataSource = self
+        
+        let t1 = Tasks(task_id: 1, task_name: "Odev zamaninda teslim edilecek.")
+        let t2 = Tasks(task_id: 2, task_name: "Eksik kalan videolar tamamlanacak.")
+        
+        tasksListesi.append(t1)
+        tasksListesi.append(t2)
     }
 
-    
-    @IBAction func btnDetay(_ sender: Any) {
-        let task = Tasks(task_id: 3, task_name: "Odev yapilacak")
-        performSegue(withIdentifier: "toDetay", sender: task)
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDetay" {
             if let task = sender as? Tasks {
@@ -28,4 +35,45 @@ class Anasayfa: UIViewController {
             
         }
     }
+}
+
+extension Anasayfa : UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Task ara: \(searchText)")
+    }
+}
+
+extension Anasayfa: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasksListesi.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasksListesi[indexPath.row]
+        
+        let hucre = tableView.dequeueReusableCell(withIdentifier: "tasksHucre") as! TasksHucre
+        hucre.labelTaskName.text = task.task_name
+        return hucre
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let task = tasksListesi[indexPath.row]
+        performSegue(withIdentifier: "toDetay", sender: task)
+        tableView.deselectRow(at: indexPath, animated: true) //secim kaldirma
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let silAction = UIContextualAction(style: .destructive, title: "Sil"){  contextualAction, view, Bool in
+            let task = self.tasksListesi[indexPath.row]
+            let alert = UIAlertController(title: "Silme İşlemi", message: " \(task.task_name!) silinsin mi?", preferredStyle: .alert)
+            
+            let iptalAction = UIAlertAction(title: "İptal", style: .cancel)
+            alert.addAction(iptalAction)
+            let evetAction = UIAlertAction(title: "Evet", style: .destructive) { action in
+                print("Task sil: \(task.task_name!)")
+            }
+            alert.addAction(evetAction)
+            self.present(alert, animated: true)
+        }
+       return UISwipeActionsConfiguration(actions: [silAction])
+    }
+
 }
