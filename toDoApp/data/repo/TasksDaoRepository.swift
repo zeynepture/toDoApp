27196fa494e80fbd67156336416fb33478be879a
Zeynep <zeynep@192.168.1.6>
2023-08-16 22:53:15 +0300

    SQLite Veritabani eklendi.

--- toDoApp/data/repo/TasksDaoRepository.swift
+++ toDoApp/data/repo/TasksDaoRepository.swift
@@ -11,29 +11,83 @@ import RxSwift
 class TasksDaoRepository {
     
     var taskListesi = BehaviorSubject<[Tasks]>(value: [Tasks]())
+    let db:FMDatabase?
+    
+    init() {
+        let dosyaYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let veritabaniURL = URL(fileURLWithPath: dosyaYolu).appendingPathComponent("todo.sqlite")
+        db = FMDatabase(path: veritabaniURL.path)
+    }
     func kaydet(task_name: String)
     {
-        print("Task olusturuluyor... Task İçeriği: \(task_name)")
+        db?.open()
+        do{
+            try db!.executeUpdate("INSERT INTO Tasks (task_name) VALUES (?)", values: [task_name])
+        }catch {
+            print(error.localizedDescription)
+        }
+        db?.close()
     }
     func guncelle(task_id:Int, task_name:String)
     {
-        print("Task guncelle : \(task_id) - \(task_name) ")
+        db?.open()
+        do{
+            try db!.executeUpdate("UPDATE Tasks SET task_name = ? WHERE task_id =? ", values: [task_name, task_id])
+        }catch{
+            print(error.localizedDescription)
+        }
+        db?.close()
     }
     
     func sil(task_id: Int) {
-        print("Task sil: \(task_id)")
+        db?.open()
+        do{
+            try db!.executeUpdate("DELETE FROM Tasks WHERE task_id = ?", values: [task_id])
+        }catch{
+            print(error.localizedDescription)
+        }
+        db?.close()
     }
     
     func arama(arananKelime: String) {
-        print("Task ara: \(arananKelime)")
+        db?.open()
+        var liste = [Tasks]()
+        
+        do{
+            let result = try db!.executeQuery("SELECT * FROM Tasks WHERE task_name like '%\(arananKelime)%'", values: nil)
+            
+            while result.next() {
+                let task_id = Int(result.string(forColumn: "task_id"))!
+                let task_name = result.string(forColumn: "task_name")!
+                
+                let task = Tasks(task_id: task_id, task_name: task_name)
+                liste.append(task)
+            }
+            taskListesi.onNext(liste)
+        }catch {
+            print(error.localizedDescription)
+        }
+        db?.close()
     }
     
     func taskYukle() {
+        db?.open()
         var liste = [Tasks]()
-        let t1 = Tasks(task_id: 1, task_name: "Odev zamaninda teslim edilecek.")
-        let t2 = Tasks(task_id: 2, task_name: "Eksik kalan videolar tamamlanacak.")
-        liste.append(t1)
-        liste.append(t2)
-        taskListesi.onNext(liste) //tetikleme
+        
+        do{
+            let result = try db!.executeQuery("SELECT * FROM Tasks", values: nil)
+            
+            while result.next() {
+                let task_id = Int(result.string(forColumn: "task_id"))!
+                let task_name = result.string(forColumn: "task_name")!
+                
+                let task = Tasks(task_id: task_id, task_name: task_name)
+                liste.append(task)
+            }
+            taskListesi.onNext(liste)
+        }catch {
+            print(error.localizedDescription)
+        }
+        db?.close()
     }
 }
--- toDoApp/ui/viewmodel/AnasayfaViewModel.swift
+++ toDoApp/ui/viewmodel/AnasayfaViewModel.swift
@@ -13,15 +13,36 @@ class AnasayfaViewModel {
     var taskListesi = BehaviorSubject<[Tasks]>(value: [Tasks]())
     
     init() {
+        veritabanikopyala()
         taskListesi = trepo.taskListesi
     }
     func sil(task_id: Int) {
         trepo.sil(task_id: task_id)
+        taskYukle()
     }
     func arama(arananKelime: String) {
         trepo.arama(arananKelime: arananKelime)
     }
     func taskYukle() {
         trepo.taskYukle()
+    }
+    func veritabanikopyala() {
+        let bundleYolu = Bundle.main.path(forResource: "todo", ofType: ".sqlite")
+        //masaustune erismek icin
+        let DosyaYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let veritabaniURL = URL(fileURLWithPath: DosyaYolu).appendingPathComponent("todo.sqlite")
+        
+        let fm = FileManager.default
+        
+        if fm.fileExists(atPath: veritabaniURL.path()) {
+            print("Veritabani mevcut.")
+        }
+        else {
+            do {
+                try fm.copyItem(atPath: bundleYolu!, toPath: veritabaniURL.path)
+            }catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
